db13adcb978de51fa8e18fc0bdfa1388387d6de0
Gkekas Dimitris <dimigkek@gmail.com>
2023-02-16 20:50:59 +0200

    Add files via upload

---
+++ backendPnI/src/main/java/com/example/demo/ServiceClass.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceClass {
+  private List<Employee> employees = new ArrayList<>();
+
+  public List<Employee> getAllEmployees() {
+    return employees;
+  }
+
+  public Employee getEmployeeById(Long id) {
+    return employees.stream().filter(employee -> employee.getId().equals(id)).findFirst().orElse(null);
+  }
+
+  public Employee updateEmployee(Long id, Employee employeeDetails) {
+    Employee employee = getEmployeeById(id);
+    if (employee != null) {
+      employee.setFirstName(employeeDetails.getFirstName());
+      employee.setLastName(employeeDetails.getLastName());
+      employee.setEmail(employeeDetails.getEmail());
+      employee.setPhone(employeeDetails.getPhone());
+      employee.setDepartment(employeeDetails.getDepartment());
+    }
+    return employee;
+  }
+}
