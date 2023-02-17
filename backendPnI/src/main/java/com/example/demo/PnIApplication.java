6df8dbb77f128f68db76404fc8f0bf1d72cfa79a
Gkekas Dimitris <dimigkek@gmail.com>
2023-02-17 20:04:24 +0200

    Add files via upload

---
+++ backendPnI/src/main/java/com/example/demo/PnIApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnIApplication.class, args);
+	}
+
+}
