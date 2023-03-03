5bd0b25b200528abdc0486e6ec21f586b8076f1f
Gkekas Dimitris <dimigkek@gmail.com>
2023-03-03 10:28:21 +0200

    Update Projects.js

--- React-frontend/pni-app/src/components/Projects.js
+++ React-frontend/pni-app/src/components/Projects.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import ProjectForm from "./ProjectForm";
 
 function Project() {
   const [projects, setProjects] = useState([]);
@@ -145,51 +146,4 @@ function Project() {
   );
 }
 
-function ProjectForm({ project, onSubmit, onCancel }) {
-  const [name, setName] = useState(project.name);
-  const [description, setDescription] = useState(project.description);
-  const [employees, setEmployees] = useState(
-    project.employees.map((employee) => employee.id)
-  );
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    onSubmit({ id: project.id, name, description, employees });
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </label>
-      <label>
-        Description:
-        <textarea
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        />
-      </label>
-      <label>
-        Employees:
-        <input
-          type="text"
-          value={employees}
-          onChange={(event) =>
-            setEmployees(event.target.value.split(",").map((e) => e.trim()))
-          }
-        />
-      </label>
-      <button type="submit">Save</button>&nbsp;
-      <button type="button" onClick={onCancel}>
-        Cancel
-      </button>
-    </form>
-  );
-}
 export default Project;
